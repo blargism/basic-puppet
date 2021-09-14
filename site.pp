6b99abe5f66e616446c646aa95b24c971ee07fec
Joe Mills <joe.mills@conecmi.com>
2021-09-14 09:01:33 -0500

    Escaping dashes

--- site.pp
+++ site.pp
@@ -1,13 +1,13 @@
 hiera_include('classes')
 
-node /^dave-primary\.[a-zA-Z0-9-_]+\.[a-zA-Z0-9-_]+/ {
+node /^dave-primary\.[a-zA-Z0-9\-_]+\.[a-zA-Z0-9\-_]+$/ {
   include role::primary
 }
 
-node /^dave-compiler\.[a-zA-Z0-9-_]+\.[a-zA-Z0-9-_]+/ {
+node /^dave-compiler\.[a-zA-Z0-9\-_]+\.[a-zA-Z0-9\-_]+$/ {
   include role::compiler
 }
 
-node /^dave-client.*\.[a-zA-Z0-9-_]+\.[a-zA-Z0-9-_]+/ {
+node /^dave-client.*\.[a-zA-Z0-9\-_]+\.[a-zA-Z0-9\-_]+$/ {
   include role::linux_client
 }
