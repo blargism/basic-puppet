e63574aacf946ed2e77102ac46da2aca7bb43afb
Joe Mills <joe.mills@ikbi.io>
2021-10-20 14:54:26 -0500

    Update site.pp

--- site.pp
+++ site.pp
@@ -3,3 +3,7 @@ hiera_include('classes')
 node /^dave-client.*\.[a-zA-Z0-9\-_]+]+$/ {
   include role::linux_client
 }
+
+node /^dave-client2.*\.[a-zA-Z0-9\-_]+]+$/ {
+  include role::linux_client
+}
