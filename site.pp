22eeda394adc11c2857b787a8e70edb2d2e65440
Joe Mills <joe.mills@ikbi.io>
2021-10-20 15:07:45 -0500

    Update site.pp

--- site.pp
+++ site.pp
@@ -7,7 +7,3 @@ node /^dave-client.*\.[a-zA-Z0-9\-_]+]+$/ {
 node /^dave-client2.*\.[a-zA-Z0-9\-_]+]+$/ {
   include role::linux_client
 }
-
-node /^dave-client3.*\.[a-zA-Z0-9\-_]+]+$/ {
-  include role::linux_client
-}
