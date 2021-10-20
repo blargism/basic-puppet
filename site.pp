233f0d5a6d3e85668e9a15e42b8dd4db11ce3820
Joe Mills <joe.mills@ikbi.io>
2021-10-20 16:45:34 -0500

    More pedantic fixes

--- site.pp
+++ site.pp
@@ -1,13 +1,13 @@
 hiera_include('classes')
 
 node 'dave-client.coalescent.technology' {
-  include role::linux_client
+  include role::linuxclient
 }
 
 node /^dave-client.*\.[a-zA-Z0-9\-_]+]+$/ {
-  include role::linux_client
+  include role::linuxclient
 }
 
 node /^dave-client2.*\.[a-zA-Z0-9\-_]+]+$/ {
-  include role::linux_client
+  include role::linuxclient
 }
